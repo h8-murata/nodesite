4597660055bb533f5bc586ced77bb1593586b436
TAKANASHI <126223927+TAICHI-TAKA@users.noreply.github.com>
2025-08-24 22:57:19 +0900

    応募フォーム修正

--- script.js
+++ script.js
@@ -114,3 +114,15 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
+document.querySelectorAll('.accordion-toggle').forEach(button => {
+    button.addEventListener('click', () => {
+        const content = button.nextElementSibling;
+        button.classList.toggle('active');
+        if (button.classList.contains('active')) {
+            content.style.maxHeight = content.scrollHeight + "px";
+        } else {
+            content.style.maxHeight = 0;
+        }
+        
+    });
+});
